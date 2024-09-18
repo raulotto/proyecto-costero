fcdba39d1c7d567faee6e233e6cbfed20d047841
clawdstack <sky.designer.wd@gmail.com>
2024-09-18 17:49:20 -0500

    creacion de paginas y rutas

--- src/global/Header.jsx
+++ src/global/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+   <header>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">Acerca de</Link></li>
+      </ul>
+    </nav>
+   </header>
   )
 }
 
